0e558bf4910e1dea541f68e62d37028bc37bde45
Francisco Paredes <frannkx@gmail.com>
2023-11-23 22:57:07 -0500

    Script funcional solo falta pulir detalles

--- vagrant/frannkx-deploy.sh
+++ vagrant/frannkx-deploy.sh
@@ -1,12 +1,10 @@
 #### Declaracion de variables requeridas
 # Repositorio
-#REPO=https://github.com/roxsross/devops-static-web.git
 URL_REPO=https://github.com/roxsross/bootcamp-devops-2023.git
 REPO=bootcamp-devops-2023
-#PROYECTO=devops-static-web
 BRANCH=clase2-linux-bash
 PROYECTO=app-295devops-travel
-DISCORD="https://discord.com/api/webhooks/1169002249939329156/7MOorDwzym-yBUs3gp0k5q7HyA42M5eYjfjpZgEwmAx1vVVcLgnlSh4TmtqZqCtbupov"
+DISCORD="https://discord.com/api/webhooks/1154865920741752872/au1jkQ7v9LgQJ131qFnFqP-WWehD40poZJXRGEYUDErXHLQJ_BBszUFtVj8g3pu9bm7h"
 R='\033[0;31m'   #'0;31' is Red's ANSI color code
 G='\033[0;32m'   #'0;32' is Green's ANSI color code
 Y='\033[1;32m'   #'1;32' is Yellow's ANSI color code
@@ -50,7 +48,7 @@ echo -e "\n `date` $B Ejecutando STAGE 1: Init - Instalando paquetes necesarios 
 # Actualizando versiones de paquetes 
 
 echo -e "\n `date` $G Actualizando sistema operativo $NOCOLOR"
-sudo apt-get update -y && sudo apt-get upgrade -y
+sudo apt-get update -y #&& sudo apt-get upgrade -y
 
 # Instalando Git
 if dpkg -s git >/dev/null 2>&1; then
@@ -175,13 +173,6 @@ fi
 #Creacion de usuarios en bd
 echo -e $G "\n Configurando base de datos" $NOCOLOR
 
-#cat > db-load-script.sql <<-EOF
-#CREATE DATABASE devopstravel;
-#CREATE USER 'codeuser'@'localhost' IDENTIFIED BY 'codepass';
-#GRANT ALL PRIVILEGES ON *.* TO 'codeuser'@'localhost';
-#FLUSH PRIVILEGES;
-#EOF
-
 cat > db-load-script.sql <<-EOF
 CREATE DATABASE devopstravel;
 CREATE USER `echo ${DB_USER}`@'localhost' IDENTIFIED BY '`echo ${DB_PASS}`';
@@ -233,23 +224,30 @@ else
 	cd $REPO
 fi
 
-#Mover al directorio donde se guardar los archivos de configuración de apache /var/www/html/https://github.com/roxsross/bootcamp-devops-2023.git
+#Mover al directorio donde se guardar los archivos de configuración de apache
 
 if git branch |grep $BRANCH >/dev/null 2>&1; then
     echo -e $G "Repositorio en rama $BRANCH"
     mv /var/www/html/index.html /var/www/html/index.html.bkp
 	cp -r $PROYECTO/* /var/www/html/
-	systemctl reload apache2
-
-    #cp -r * /var/www/html/
-    #sed -i 's/xxx.xxx.xxx.xxx/localhost/g /var/www/html/index.php
 else
     echo -e $R "El repo no esta en $BRANCH" $NOCOLOR
 	git checkout $BRANCH #Ajuste particulpar para acceder al branch en donde esta la carpeta de la web
 	mv /var/www/html/index.html /var/www/html/index.html.bck
 	cp -r $PROYECTO/* /var/www/html/
-	systemctl reload apache2
 fi
+
+############################# STAGE 3: Deploy ########################################
+echo -e $G "Cargando data y configurando aplicación $BRANCH" $NOCOLOR
+
+mysql < $PROYECTO/database/devopstravel.sql
+sed -i s/codeuser/$DB_USER/g /var/www/html/config.php
+sed -i s/'""'/$DB_PASS/g /var/www/html/config.php
+
+#Ajustar el config de php para que soporte los archivos dinamicos de php agreganfo index.php
+
+sed -i s/DirectoryIndex\ index.html\ index.cgi\ index.pl\ index.php\ index.xhtml\ index.htm/DirectoryIndex\ index.php\ index.html\ index.cgi\ index.pl\ index.xhtml\ index.htm/g  /etc/apache2/mods-enabled/dir.conf
+systemctl reload apache2
 
 #Testear existencia del codigo de la aplicación
 if curl localhost >/dev/null 2>&1; then
@@ -258,8 +256,41 @@ else
 	echo -e $R "No esta funcional el codigo $BRANCH" $NOCOLOR
 fi
 
-#Ajustar el config de php para que soporte los archivos dinamicos de php agreganfo index.php
-
 #Testear la compatibilidad -> ejemplo http://localhost/info.php
-
+curl localhost/info.php
+curl localhost/info.php >> $LOG
 #Si te muestra resultado de una pantalla informativa php , estariamos funcional para la siguiente etapa.
+
+############################# STAGE 4: Notify ########################################
+
+#El status de la aplicacion si esta respondiendo correctamente o esta fallando debe reportarse via webhook al canal de discord #deploy-bootcamp
+#Informacion a mostrar : Author del Commit, Commit, descripcion, grupo y status
+
+REPO_NAME=$(basename $(git rev-parse --show-toplevel))
+REPO_URL=$(git remote get-url origin)
+WEB_URL="localhost"
+# Realiza una solicitud HTTP GET a la URL
+HTTP_STATUS=$(curl -Is "$WEB_URL" | head -n 1)
+
+# Verifica si la respuesta es 200 OK (puedes ajustar esto según tus necesidades)
+if [[ "$HTTP_STATUS" == *"200 OK"* ]]; then
+  # Obtén información del repositorio
+    DEPLOYMENT_INFO2="Despliegue del repositorio $REPO_NAME: "
+    DEPLOYMENT_INFO="La página web $WEB_URL está en línea."
+    COMMIT="Commit: $(git rev-parse --short HEAD)"
+    AUTHOR="Autor: $(git log -1 --pretty=format:'%an')"
+    DESCRIPTION="Descripción: $(git log -1 --pretty=format:'%s')"
+else
+  DEPLOYMENT_INFO="La página web $WEB_URL no está en línea."
+fi
+
+# Construye el mensaje
+MESSAGE="$DEPLOYMENT_INFO2\n$DEPLOYMENT_INFO\n$COMMIT\n$AUTHOR\n$REPO_URL\n$DESCRIPTION\nEstudiante Individual: Francisco Paredes"
+echo -e $G "Notificando al webhook de Discord el siguiente mensaje \n $MESSAGE" $NOCOLOR
+
+
+# Envía el mensaje a Discord utilizando la API de Discord
+curl -X POST -H "Content-Type: application/json" \
+     -d '{
+       "content": "'"${MESSAGE}"'"
+     }' "$DISCORD"
